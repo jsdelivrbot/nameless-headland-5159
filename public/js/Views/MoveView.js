585b810a81e28e89999041b624dd34efa71ab4d8
Ethan H. Jurman <ehj2229@rit.edu>
2014-08-25 10:46:25 -0400

    fixed styling

--- public/js/Views/MoveView.js
+++ public/js/Views/MoveView.js
@@ -1,7 +1,7 @@
 MoveView = Backbone.Marionette.ItemView.extend({
   tagName: 'tr class="move-element" class="<%= args.move_type %>"',
   template: Templite(
-    '<td><%= args.name %></td><td><%= args.exec %></td><td><%= args.notes %></td>'
+    '<td class="move-name"><%= args.name %></td><td class="move-exec"><%= args.exec %></td><td class="move-note"><%= args.note %></td>'
   ),
   initialize: function() {
     console.log("move rendered");
