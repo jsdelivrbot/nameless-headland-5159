09dffd5165a182287ccf62cc2df511a810476415
Jesse Jurman <j.r.jurman@gmail.com>
2014-08-24 22:13:00 -0400

    made character name the id

--- public/js/Models/Character.js
+++ public/js/Models/Character.js
@@ -1,7 +1,8 @@
 Character = Backbone.Model.extend({
+  idAttribute: "name",
   initialize: function(options) {
     console.log("Making Character");
-    console.log("Found Character with id: "+this.get("id"));
+    console.log("Found Character with id: "+this.id);
     console.log("Found Character with name: "+this.get("name"));
-  },
+  }
 });
